4e76ce45d7f1f90df4f5b7cab0fe1aef9223f70c
ZMathias <zagoni.matyas@yandex.ru>
2022-03-30 07:57:10 +0300

    Added proper error flow with logging. The process now launches directly into cmd. Implemented proper cleanup with improper exit protection.

--- ResourcePacker.cpp
+++ ResourcePacker.cpp
@@ -8,17 +8,26 @@
 #include <zip.h>
 #include "resource.h"
 
+// usual and unusual cleanup state defines
+#define CLEANUP_USUAL 0
+#define CLEANUP_UNUSUAL 1
+
+
+constexpr std::string_view archive_name{"java.zip"};
+
+//global StdOut buffer handle
+HANDLE hConsole;
+
 std::pair<char*, DWORD> get_resource(const int resource_name, const int resource_type)
 {
 	std::pair<char*, DWORD> final_data;
 	HMODULE h_module = nullptr;
 	GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS, reinterpret_cast<LPCTSTR>(get_resource), &h_module);
 
-	const HRSRC h_resource = FindResource(h_module, MAKEINTRESOURCE(resource_name), MAKEINTRESOURCE(resource_type));
+	HRSRC h_resource = FindResource(h_module, MAKEINTRESOURCE(resource_name), MAKEINTRESOURCE(resource_type));
 	if (h_resource == nullptr)
 	{
-		printf("error while finding resource: %lu", GetLastError());
-		return {nullptr, 0};
+		throw std::runtime_error("Error while finding resource: " + std::to_string(GetLastError()));
 	}
 
 	const HGLOBAL h_pre_data = LoadResource(h_module, h_resource);
@@ -27,6 +36,33 @@ std::pair<char*, DWORD> get_resource(const int resource_name, const int resource
 	final_data.second = SizeofResource(h_module, h_resource);
 	final_data.first = static_cast<char*>(LockResource(h_pre_data));
 	return final_data;
+}
+
+void LogInfo(std::ostream& ostr, const std::string_view& str)
+{
+	ostr << "[";
+	SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE);
+	ostr << "INFO";
+	SetConsoleTextAttribute(hConsole, 15);
+	ostr << "] " << str;
+}
+
+void LogWarning(std::ostream& ostr, const std::string_view& str)
+{
+	ostr << "[";
+	SetConsoleTextAttribute(hConsole, 14 | FOREGROUND_INTENSITY);
+	ostr << "WARN";
+	SetConsoleTextAttribute(hConsole, 15);
+	ostr << "] " << str;
+}
+
+void LogError(std::ostream& ostr, const std::string_view& str)
+{
+	ostr << "[";
+	SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
+	ostr << "ERROR";
+	SetConsoleTextAttribute(hConsole, 15);
+	ostr << "] " << str;
 }
 
 bool ExtractFile(const std::string_view file_name)
@@ -38,9 +74,8 @@ bool ExtractFile(const std::string_view file_name)
 	// Check for errors
 	if (err != 0)
 	{
-		std::cout << "Error opening zip file: " << err << std::endl;
 		zip_close(zip);
-		return EXIT_FAILURE;
+		throw std::runtime_error("Failed to open zip file with error: " + std::to_string(err) + "\n");
 	}
 
 	const int numFiles = zip_get_num_files(zip);
@@ -73,12 +108,7 @@ bool ExtractFile(const std::string_view file_name)
 			zip_fclose(file);
 		}
 	}
-	/*//close the zip file and check for errors
-	if (zip_close(zip) != 0)
-	{
-		std::cerr << "Error closing zip file\n";
-		return EXIT_FAILURE;
-	}*/
+	
 	zip_discard(zip);
 	return EXIT_SUCCESS;
 }
@@ -89,11 +119,11 @@ BOOL SpawnJavaProcess(char* args)
 	PROCESS_INFORMATION processInfo{};
 
 	ZeroMemory(&startupInfo, sizeof startupInfo);
-	startupInfo.cb = sizeof(startupInfo);
+	startupInfo.cb = sizeof startupInfo;
 	ZeroMemory(&processInfo, sizeof processInfo);
 
 	const BOOL result = CreateProcessA(
-		R"(C:\Windows\System32\cmd.exe)",
+		nullptr /*R"(C:\Windows\System32\cmd.exe)"*/,
 		args,
 		nullptr,
 		nullptr,
@@ -104,12 +134,30 @@ BOOL SpawnJavaProcess(char* args)
 		&startupInfo,
 		&processInfo
 	);
+	// Check if successful
+	if (result == 0) throw std::runtime_error("Failed to spawn java process with error: " + std::to_string(GetLastError()) + ".\n");
+
+	LogInfo(std::clog, "Successfully spawned java process. Waiting until it finishes...\n");
+
+	// use processInfo to wait for the process to finish
+	WaitForSingleObject(processInfo.hProcess, INFINITE);
+
 	CloseHandle(processInfo.hProcess);
 	CloseHandle(processInfo.hThread);
-	return result;
+	return EXIT_SUCCESS;
 }
 
-BOOL SetJAVAHOME(const char* path)
+BOOL SetJDKAttributes(const DWORD& attributes)
+{
+	if (SetFileAttributesA("jdk", attributes) == 0)
+	{
+		LogWarning(std::clog, "Failed to set JDK attributes.\n");
+		return FALSE;
+	}
+	return TRUE;
+}
+
+/*BOOL SetJAVAHOME(const char* path)
 {
 	std::string java_home_path(path);
 	java_home_path += "\\jdk";
@@ -136,64 +184,114 @@ BOOL SetJAVAHOME(const char* path)
 		return false;
 	}
 	return true;
-}
+}*/
 
-std::string findJar()
+std::string FindJar()
 {
 	WIN32_FIND_DATAA find_data{};
-	if (INVALID_HANDLE_VALUE == FindFirstFileA("*.jar", &find_data))
+	HANDLE file_handle = FindFirstFileA("*.jar", &find_data);
+	if (INVALID_HANDLE_VALUE == file_handle)
 	{
 		return {};
 	}
+	CloseHandle(file_handle);
 	return find_data.cFileName;
 }
 
+void DumpJDK()
+{
+	std::ofstream java_file(archive_name.data(), std::ios_base::binary);
+	const auto text = get_resource(DEMO_TEXT, IDR_TEXT_FILE1);
+	java_file.write(text.first, text.second);
+	java_file.close();
+}
+
+void Cleanup(const unsigned int& state)
+{
+	if (state == CLEANUP_UNUSUAL && GetFileAttributesA(archive_name.data()) != INVALID_FILE_ATTRIBUTES)
+	{
+		if (DeleteFileA(archive_name.data()) == 0) LogWarning(std::clog, "Partial cleanup: java archive was not found. (" + std::to_string(GetLastError()) + ")\n");
+	}
+
+	if (GetFileAttributesA("jdk") != INVALID_FILE_ATTRIBUTES)
+	{
+		SHFILEOPSTRUCTA file_op{};
+
+		file_op.wFunc = FO_DELETE;
+		file_op.pFrom = "jdk\0";
+		file_op.fFlags = FOF_SILENT | FOF_NOCONFIRMATION | FOF_NOERRORUI | FOF_NOCONFIRMMKDIR;
+
+
+		const DWORD error = SHFileOperationA(&file_op);
+		if (error != 0)
+		{
+			LogWarning(std::clog, "Encountered an error while deleting the JDK: " + std::to_string(error) + ".\n");
+			SetJDKAttributes(FILE_ATTRIBUTE_NORMAL);
+		}
+	}
+	else LogWarning(std::clog, "Partial cleanup: JDK was not found.\n");
+}
+
+
 int main(int argc, char* argv[])
 {
-	//print out all arguments
-	for (int i{}; i < argc; ++i)
+	try
 	{
-		std::cout << argv[i] << '\n';
-	}
+		hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
-	// Check if the jdk folder exists
-	if (GetFileAttributesA("jdk") == INVALID_FILE_ATTRIBUTES)
-	{
-		const std::string file_name("java.zip");
-		std::ofstream java_file(file_name, std::ios_base::binary);
-		const auto text = get_resource(DEMO_TEXT, IDR_TEXT_FILE1);
-		java_file.write(text.first, text.second);
-		java_file.close();
-
-		if (!ExtractFile(file_name))
+		//print out all arguments
+		for (int i{}; i < argc; ++i)
 		{
-			std::cout << "Successfully extracted the portable java runtime" << std::endl;
+			std::cout << argv[i] << '\n';
 		}
 
-		if (DeleteFile(L"java.zip") == 0)
+		// Check if the jdk folder exists
+		if (GetFileAttributesA("jdk") == INVALID_FILE_ATTRIBUTES)
 		{
-			std::cerr << "Error deleting \"java.zip\": " << GetLastError() << std::endl;
+			DumpJDK();
+			if (!ExtractFile(archive_name)) LogInfo(std::clog, "Successfully extracted the portable java runtime.\n");
+			if (DeleteFileA(archive_name.data()) == 0) LogWarning(std::clog, "Error deleting java.zip: " + std::to_string(GetLastError()) + '\n');
 		}
+
+		SetJDKAttributes(FILE_ATTRIBUTE_HIDDEN | FILE_ATTRIBUTE_SYSTEM);
+
+		std::string progr_args{ "jdk\\bin\\java.exe -jar " };
+		//std::string progr_args{"/k jdk\\bin\\java.exe -Xmx2048M -Xms512M -Dspring.profiles.active=dev -Dspring.config.name=application,environment -Dspring.config.additional-location=config\\ -Dlogging.config=config\\logback.xml -jar "};
+
+		const std::string jar_name = FindJar();
+
+		if (jar_name.empty()) throw std::runtime_error("Failed to find jar file.\n");
+
+		progr_args += jar_name + " ";
+
+		if (argc >= 2)
+		{
+			const std::string tmp = argv[1];
+			progr_args += tmp.substr(1, std::string::npos) + " ";
+		}
+
+		for (int i = 2; i < argc; ++i)
+		{
+			progr_args += argv[i];
+			progr_args += ' ';
+		}
+
+		const auto args = new char[progr_args.size()]{};
+		std::ranges::copy(progr_args, args);
+		args[progr_args.size()] = '\0';
+
+		LogInfo(std::clog, "Attempting to launch " + jar_name + "...\n");
+
+		//spawns the java process and waits for it to finish
+		SpawnJavaProcess(args);
+
+		LogInfo(std::clog, "Detected thread exit signal. Cleaning up and exiting...\n");
+
+		Cleanup(CLEANUP_USUAL);
 	}
-
-	std::string progr_args{"/k jdk\\bin\\java.exe -Xmx2048M -Xms512M -Dspring.profiles.active=dev -Dspring.config.name=application,environment -Dspring.config.additional-location=config\\ -Dlogging.config=config\\logback.xml -jar "};
-	progr_args += findJar() + " ";
-
-	if (argc >= 2)
+	catch (const std::exception& e)
 	{
-		const std::string tmp = argv[1];
-		progr_args += tmp.substr(1, std::string::npos) + " ";
+		LogError(std::clog, e.what());
+		Cleanup(CLEANUP_UNUSUAL);
 	}
-
-	for (int i = 2; i < argc; ++i)
-	{
-		progr_args += argv[i];
-		progr_args += ' ';
-	}
-	char* args = new char[progr_args.size() + 1];
-	memcpy(args, progr_args.c_str(), progr_args.size() + 1);
-	args[progr_args.size()] = '\0';
-
-	SpawnJavaProcess(args);
-	putchar('\n');
 }
